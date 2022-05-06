48cd71b9eb2af2956d6fb0fb9496b0f2eb796029
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2022-05-06 14:48:00 +0200

    Add files via upload

--- manu.js
+++ manu.js
@@ -5,9 +5,9 @@ navicon.addEventListener('click', function (event) {
     nav.classList.toggle('nav-active')
     navicon.classList.toggle('icon-th-list-outline')
     navicon.classList.toggle('icon-th-list-outline')
-})
+});
 $(function() {
 	$('.gallery').pignoseGallery({
 		thumbnails: '.gallery-thumbnails'
 	});
-});	
+});
